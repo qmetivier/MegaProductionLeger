1e35e7e2cfa3d8ef5c7c95ddc53bf7040d0f634d
qmetivier <q.metivier@iia-laval.fr>
2019-05-06 17:22:15 +0200

    Correction fluxJsonController
    Correctiàon erreur dans le fichier fluxJsonController

--- megaCasting/controllers/fluxJsonController.php
+++ megaCasting/controllers/fluxJsonController.php
@@ -6,12 +6,13 @@ include '../services/function.php';
 //Verifie que la personne se connecte bien avec des info en Methode POST
 if (!isset($_POST["identifiant"]) && !isset($_POST["password"])) echo "Error Request";
 else{
+	var_dump($_POST);
 	if (requeteSql::getPartenaire($_POST["identifiant"], $_POST["password"])) {
 		//on récupère les offres de castings
 		$content = requeteSql::getAlloffres();
 		//On affiche sous forme Json la requete;
 		echo( json_encode($content));
-	}else
+	}
 }
 
 
